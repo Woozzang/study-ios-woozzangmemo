9edcf0b5ddd3df4600047a9859821c27c0a358f8
Woozzang <wozzaang@gmail.com>
2021-01-30 21:17:20 +0900

    Add Model Class

---
+++ WoozzangMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  WoozzangMemo
+//
+//  Created by Woochan Park on 2021/01/30.
+//
+
+import Foundation
+
+class Memo {
+  var content: String
+  var insertDate: Date
+  
+  init(content: String) {
+    self.content = content
+    insertDate = Date()
+  }
+  
+  static var dummyMemoList = [
+    Memo(content: "Woochan Park"),
+    Memo(content: "🐶 💜 🐻")
+  ]
+}
