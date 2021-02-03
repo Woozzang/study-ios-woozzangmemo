467b5047e741d741c8b5586d86c38d9004768254
Woozzang <woozzaang@gmail.com>
2021-02-03 22:48:38 +0900

    Add Detail Memo View

---
+++ WoozzangMemo/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  WoozzangMemo
+//
+//  Created by Woochan Park on 2021/02/03.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+      // Do any additional setup after loading the view.
+  }
+  
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      // Get the new view controller using segue.destination.
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
+
+extension DetailViewController: UITableViewDataSource {
+  
+  /* 테이블 뷰가 표시할 셀 숫자를 물어볼 때 호출*/
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  /* 테이블 뷰가 어떤 셀을 표시할 지 물어볼 때 호출*/
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.row {
+    
+    case 0:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+      return cell
+    
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+      return cell
+      
+    default:
+      fatalError()
+    }
+  }
+  
+  
+}
